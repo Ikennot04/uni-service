66f141e6f32d2658bc63ef39b198003ea2edfb12
John Anthony Rico <johnrico364@gmail.com>
2025-12-13 01:23:15 +0800

    Add chat messages schema

---
+++ server/modules/chat_messages/chat_message.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const chatMessageSchema = new mongoose.Schema(
+  {
+    chat_id: {type: String, required: true, ref:'Chat'},
+    sender_id: {type: String, required: true, ref: 'User'},
+    receiver_id: {type: String, required: true, ref: 'User'},
+    message: {type: String, required: true},
+    is_read: {type: Boolean, default: false},
+  },
+  {
+    timestamps: true,
+  }
+)
